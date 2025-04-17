7afa2f64d8e79907254386fbb7420b943d30ece7
smikulas <miksza01@gmail.com>
2025-04-17 19:43:06 +0200

    build target harness without seeds

---
+++ Part1/run . w_o_corpus .sh
@@ -0,0 +1,7 @@
+git clone --single-branch --branch CS412Lab2Part1 https://github.com/google/oss-fuzz && cd oss-fuzz
+python3 infra/helper.py build_image libpng
+python3 infra/helper.py build_fuzzers libpng
+mkdir build/out/corpus/
+python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus
+python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
+python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus/ --fuzz-target libpng_read_fuzzer
