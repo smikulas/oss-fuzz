117880d3d8809f724e4faf69c3fb1898e5df75ab
Sbzro12345 <72307706+Sbzro12345@users.noreply.github.com>
2025-05-01 18:46:15 +0200

    fixed timer

--- Part1/run . w_o_corpus .sh
+++ Part1/run . w_o_corpus .sh
@@ -2,6 +2,6 @@ git clone --single-branch --branch CS412Lab2Part1 https://github.com/smikulas/os
 python3 infra/helper.py build_image libpng
 python3 infra/helper.py build_fuzzers libpng
 mkdir build/out/corpus/
-python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus -- -max_total_time=14400
+python3 infra/helper.py run_fuzzer libpng --corpus-dir build/out/corpus libpng_read_fuzzer -- -max_total_time=14400
 python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
 python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus/ --fuzz-target libpng_read_fuzzer
