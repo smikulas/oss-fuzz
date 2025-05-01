5f8da11165ca2264bf19ee183e1d6df7717e073e
Sbzro12345 <72307706+Sbzro12345@users.noreply.github.com>
2025-05-01 16:43:16 +0200

    add timer for 4hrs

--- Part1/run . w_corpus .sh
+++ Part1/run . w_corpus .sh
@@ -2,6 +2,6 @@ git clone https://github.com/google/oss-fuzz && cd oss-fuzz
 python3 infra/helper.py build_image libpng
 python3 infra/helper.py build_fuzzers libpng
 mkdir build/out/corpus/
-python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus
+python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus -- -max_total_time=14400
 python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
 python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus/ --fuzz-target libpng_read_fuzzer
