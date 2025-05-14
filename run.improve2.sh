9ac69834c88d5a44992ae62e3f2f4d405a413cd1
Sbzro12345 <72307706+Sbzro12345@users.noreply.github.com>
2025-05-14 23:22:46 +0200

    Delete run.improve2.sh

--- run.improve2.sh
+++
@@ -1,7 +0,0 @@
-git clone --single-branch --branch CS412Lab2Part3-new-seed https://github.com/smikulas/oss-fuzz.git && cd oss-fuzz
-echo "y" | python3 infra/helper.py build_image libpng
-python3 infra/helper.py build_fuzzers libpng
-mkdir build/out/corpus/
-python3 infra/helper.py run_fuzzer libpng --corpus-dir build/out/corpus libpng_read_fuzzer -- -max_total_time=14400
-python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
-python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus/ --fuzz-target libpng_read_fuzzer
