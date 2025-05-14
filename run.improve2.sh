1c24da2ce7b48c5978bbde10387e7807f8334556
John Taylor <john.taylor@epfl.ch>
2025-05-14 19:56:47 +0200

    Change branch pointed too in run.improve2.sh

--- run.improve2.sh
+++ run.improve2.sh
@@ -1,4 +1,4 @@
-git clone --single-branch --branch mng https://github.com/smikulas/oss-fuzz.git && cd oss-fuzz
+git clone --single-branch --branch CS412Lab2Part3-new-seed https://github.com/smikulas/oss-fuzz.git && cd oss-fuzz
 python3 infra/helper.py build_image libpng
 python3 infra/helper.py build_fuzzers libpng
 mkdir build/out/corpus/
